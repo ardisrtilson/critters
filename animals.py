21f81fc644a36970dbf46b30f350172255bd10cb
ardisrtilson <email@example.com>
2020-10-20 12:32:29 -0500

    Chapters 1-3

---
+++ animals.py
@@ -0,0 +1,122 @@
+from datetime import date
+
+class carp(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class trout(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class angler(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class shark(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class rainbow(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class copperhead(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class viper(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class boa(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class adder(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class garter(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class rhino(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+class emu(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+class bear(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+class ostritch(): 
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+class zebra():
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+morris = carp("Morris", "Carp")
+frank = trout("Frank", "Trout")
+jill = angler("Jill", "Angler")
+bill = shark("Bill", "Shark")
+sid = rainbow("Sid", "Rainbow")
+chuck = copperhead("Chuck", "Copperhead")
+brock = viper("Brock", "Viper")
+emmy = boa("Emmy", "Boa")
+cassie = adder("Cassie", "Adder")
+flora = garter("Flora", "Garter")
+randy = rhino("Randy", "Rhino")
+sandy = emu("Sandy", "Emu")
+brandy = bear("Brandy", "Bear")
+becca = ostritch("Becca", "Ostritch")
+sara = zebra("Sara", "Zebra")
